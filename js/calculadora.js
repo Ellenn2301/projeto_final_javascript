9c851927a45b50c1d3dd08c8fd2f3cfb6633b4cd
Ellenn2301 <ellenncristina99@gmail.com>
2023-11-27 21:14:51 -0300

    projeto

---
+++ js/calculadora.js
@@ -0,0 +1,47 @@
+function somar() {
+    const numero1 = parseFloat(document.getElementById("numero1Soma").value);
+    const numero2 = parseFloat(document.getElementById("numero2Soma").value);
+    // evite mexer no código acima!
+  
+    const resultadoSoma = numero1 + numero2;
+  
+    /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
+    Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
+    document.getElementById("resultadoSoma").innerHTML = resultadoSoma;
+}
+
+function subtrair() {
+    const numero1 = parseFloat(document.getElementById("numero1Sub").value);
+    const numero2 = parseFloat(document.getElementById("numero2Sub").value);
+    // evite mexer no código acima!
+
+   const resultadoSubtracao = numero1 - numero2;
+
+    /* use as constantes numero 1 e numero 2 e realize a subtração entre elas.
+        Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
+    document.getElementById("resultadoSub").innerHTML = resultadoSubtracao;
+}
+
+function multiplicar() {
+    const numero1 = parseFloat(document.getElementById("numero1Mult").value);
+    const numero2 = parseFloat(document.getElementById("numero2Mult").value);
+    // evite mexer no código acima!
+
+    const resultadoMultiplicacao = numero1 * numero2;
+
+    /* use as constantes numero 1 e numero 2 e realize a multiplicação entre elas.
+        Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
+    document.getElementById("resultadoMult").innerHTML = resultadoMultiplicacao;
+}
+
+function dividir() {
+    const numero1 = parseFloat(document.getElementById("numero1Div").value);
+    const numero2 = parseFloat(document.getElementById("numero2Div").value);
+    // evite mexer no código acima!
+
+    const resultadoDivisao = numero1 / numero2;
+
+    /* use as constantes numero 1 e numero 2 e realize a divisão entre elas.
+        Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
+    document.getElementById("resultadoDiv").innerHTML = resultadoDivisao;
+}
