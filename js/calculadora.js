36a57768299b8ac4269fa9ff4f7da7e0450ec483
Ellenn2301 <ellenncristina99@gmail.com>
2023-11-30 12:13:57 -0300

    projeto-f

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,13 +1,13 @@
 function somar() {
     const numero1 = parseFloat(document.getElementById("numero1Soma").value);
     const numero2 = parseFloat(document.getElementById("numero2Soma").value);
-    // evite mexer no código acima!
-  
-    const resultadoSoma = numero1 + numero2;
-  
-    /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
-    Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
-    document.getElementById("resultadoSoma").innerHTML = resultadoSoma;
+        // evite mexer no código acima!
+
+        const resultadoSoma = numero1 + numero2;
+
+        /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
+        Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
+        document.getElementById("resultadoSoma").innerHTML = resultadoSoma;
 }
 
 function subtrair() {
@@ -15,7 +15,7 @@ function subtrair() {
     const numero2 = parseFloat(document.getElementById("numero2Sub").value);
     // evite mexer no código acima!
 
-   const resultadoSubtracao = numero1 - numero2;
+    const resultadoSubtracao = numero1 - numero2;
 
     /* use as constantes numero 1 e numero 2 e realize a subtração entre elas.
         Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
--- js/media.js
+++ js/media.js
@@ -16,34 +16,44 @@ function calcularMedia() {
     const nota2 = parseFloat(document.getElementById("nota2").value);
     const nota3 = parseFloat(document.getElementById("nota3").value);
     const nota4 = parseFloat(document.getElementById("nota4").value);
-  
+
+    
     let mensagem = "";
     // evite mexer no código acima!
 
+    let resultadoMedia = ((nota1 + nota2 + nota3 + nota4) / 4);
+    
+    if(resultadoMedia == 0){
+      //console.log(`Infelizmente você zerou a prova :( `);
+      mensagem = `Infelizmente você zerou a prova :( `
+    } 
+    else if(resultadoMedia >= 0.1 && resultadoMedia <= 3){
+      //console.log(`Caramba, deu ruim, você obteve media ${resultadoMedia}! Estude mais e tente novamente!`);
+      mensagem = `Caramba, deu ruim, você obteve média ${resultadoMedia}! Estude mais e tente novamente.`
+    } 
+    else if(resultadoMedia >=3.1 && resultadoMedia <= 5.9){
+      //console.log(`Você obteve media ${resultadoMedia}! Falta pouco para a média.`);
+      mensagem = `Você obteve média ${resultadoMedia}! Falta pouco para alcançar a média.`
+    }
+    else if(resultadoMedia >= 6 && resultadoMedia <= 7){
+      //console.log(`Você está na média com ${resultadoMedia}`);
+      mensagem = `Você está na média com ${resultadoMedia}. Pode melhorar! :) `
+    }
+    else if(resultadoMedia >= 7.1 && resultadoMedia <= 9.9){
+      //console.log(`Notão! Sua média é ${resultadoMedia}!`);
+      mensagem = `Notão! Sua média é ${resultadoMedia}!`
+    }
+    else{
+      //console.log(`Hoje é seu aniversário? Pq você ta de parabéns! 10 de média`);
+      mensagem = `Hoje é seu aniversário? Porque você está de parabéns! 10 na média.`
+    }
+
     // evite mexer na linha de código abaixo!
     document.getElementById("situacaoAluno").innerHTML = mensagem; //mensagem
+
 }
 
-  let resultadoMedia = calcularMedia((nota1 + nota2 + nota3 + nota4) / 4);
-
-  if(resultadoMedia == 0){
-    console.log("Infelizmente você zerou a prova :( ");
-  } 
-  else if(resultadoMedia >= 0.1 || resultadoMedia <= 3){
-    console.log("Caramba, deu ruim, você obteve media ${resultadoMedia}! Estude mais e tente novamente!");
-  } 
-  else if(resultadoMedia >=3.1 || resultadoMedia <= 5.9){
-    console.log("Você obteve media ${resultadoMedia}! Falta pouco para a média.");
-  }
-  else if(resultadoMedia >= 6 || resultadoMedia <= 7){
-    console.log("Você está na média com ${resultadoMedia}");
-  }
-  else if(resultadoMedia >= 7.1 || resultadoMedia <= 9.9){
-    console.log("Notão! Sua média é ${resultadoMedia}!");
-  }
-  else{
-    console.log("Hoje é seu aniversário? Pq você ta de parabéns! 10 de média");
-  }
+  
   
 
 
